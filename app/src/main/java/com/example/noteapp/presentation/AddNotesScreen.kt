5e1352cfc5020eb7d6b879637f748992fa25a83b
Cephas Gweth <cfreeka18@gmail.com>
2024-01-13 12:14:35 +0300

    Add note screen

--- app/src/main/java/com/example/noteapp/presentation/AddNotesScreen.kt
+++ app/src/main/java/com/example/noteapp/presentation/AddNotesScreen.kt
@@ -1,14 +1,83 @@
 package com.example.noteapp.presentation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddNotesScreen(
     state: NoteState,
     navController: NavController,
     onEvent: (NotesEvent) -> Unit
 ) {
+
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                onEvent(
+                    NotesEvent.SaveData(
+                        title = state.title.value,
+                        description = state.description.value
+                    )
+                )
+                navController.popBackStack()
+
+            }) {
+                Icon(imageVector = Icons.Default.Check, contentDescription = "Save Note")
+
+            }
+        }
+
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+
+            ) {
+            TextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                value = state.title.value,
+                onValueChange = { state.title.value = it },
+                textStyle = TextStyle(
+                    fontSize = 17.sp
+                )
+            )
+            BasicTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                value = state.title.value,
+                onValueChange = { state.title.value = it },
+                textStyle = TextStyle(
+                    fontSize = 17.sp
+                )
+
+            )
+
+
+        }
+    }
+
 
 }
